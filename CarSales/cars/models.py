e1d926e9af72a5d72ecab831e8513c4ac8244e0b
AnanyaSubburathinam <AnanyaSubburathinam@users.noreply.github.com>
2018-04-23 23:52:34 -0400

    Added RELIABILITY SCORE to the car's details. Added hr line to separate car details from dealer details

--- CarSales/cars/models.py
+++ CarSales/cars/models.py
@@ -9,10 +9,11 @@ class Car(models.Model):
     car_image = ImageField('Image', upload_to = 'cars/')  
     car_web = models.URLField('Website', max_length = 200)    
     car_picked = models.BooleanField('Is Picked', default = False)    
-    car_description = models.CharField('Description', default = 'Good Car!', max_length = 200)
+    car_reliability = models.IntegerField('Reliability', default = 5)
     car_price = models.DecimalField('Price', max_digits = 8, decimal_places = 2)
     car_make = models.CharField('Make', max_length = 200, choices = ch.MAKE_CHOICES)    
-    car_model = models.CharField('Model', max_length = 200, choices = ch.MODEL_CHOICES)                     
+    car_model = models.CharField('Model', max_length = 200, choices = ch.MODEL_CHOICES)  
+    car_description = models.CharField('Description', default = 'Good Car!', max_length = 200)
     
     #dealer info    
     dealer_web = models.URLField('Dealer Website', max_length = 200)     
